cd8ca1788251e0a60421613f89115c06c6d5cde9
YagoAyala <yago.ayala@outlook.com>
2022-11-07 18:10:37 -0300

    fix: fixed font awesome icons bug

--- yago-todo-list/src/components/TodosList.js
+++ yago-todo-list/src/components/TodosList.js
@@ -1,6 +1,12 @@
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faCheckCircle, faEdit, faTrash} from "@fortawesome/free-solid-svg-icons"
 
 const TodosList = ({ todos, setTodos }) => {
+
+    const handleDelete = ({ id }) => {
+        
+    }
     return (
         <div>
             {todos.map((todo) => (
@@ -10,6 +16,17 @@ const TodosList = ({ todos, setTodos }) => {
                     className="list" 
                     onChange={(event) => event.preventDefault()} 
                     />
+                    <div>
+                    <button className="button-complete task-button">
+                        <FontAwesomeIcon icon={faCheckCircle} />
+                    </button>
+                    <button className="button-edit task-button">
+                        <FontAwesomeIcon icon={faEdit} />
+                    </button>
+                    <button className="button-delete task-button" onClick={() => handleDelete(todo)}>
+                        <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                </div>
                 </li>
             ))}
         </div>
