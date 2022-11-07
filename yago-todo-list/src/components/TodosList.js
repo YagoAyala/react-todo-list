ef3b6fadbf7297c1abe5e478a0037516ac06f139
YagoAyala <yago.ayala@outlook.com>
2022-11-07 18:29:08 -0300

    fix: handled Forms map error

--- yago-todo-list/src/components/TodosList.js
+++ yago-todo-list/src/components/TodosList.js
@@ -2,10 +2,27 @@ import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faCheckCircle, faEdit, faTrash} from "@fortawesome/free-solid-svg-icons"
 
-const TodosList = ({ todos, setTodos }) => {
+const TodosList = ({ todos, setTodos, setEditTodo }) => {
+
+    const handleComplete = (todo) => {
+        setTodos(
+            todos.map((item) => {
+                if(item.id === todo.id) {
+                    return {...item, completed: !item.completed}
+                }
+                return item
+            })
+        )
+    }
+
+    const handleEdit = ({ id }) => {
+        const findTodo = todos.find((todo) => todo.id === id);
+        setEditTodo(findTodo);
+    }
+
 
     const handleDelete = ({ id }) => {
-        
+        setTodos(todos.filter((todo) => todo.id !== id))
     }
     return (
         <div>
@@ -17,13 +34,15 @@ const TodosList = ({ todos, setTodos }) => {
                     onChange={(event) => event.preventDefault()} 
                     />
                     <div>
-                    <button className="button-complete task-button">
+                    <button className="button-complete task-button" onClick={() => handleComplete(todo)}>
                         <FontAwesomeIcon icon={faCheckCircle} />
                     </button>
-                    <button className="button-edit task-button">
+                    <button className="button-edit task-button" onClick={() => handleEdit(todo)}>
                         <FontAwesomeIcon icon={faEdit} />
                     </button>
-                    <button className="button-delete task-button" onClick={() => handleDelete(todo)}>
+                    <button className="button-delete task-button" 
+                    onClick={() => handleDelete(todo)}
+                    >
                         <FontAwesomeIcon icon={faTrash} />
                     </button>
                 </div>
